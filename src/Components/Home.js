37e0c7503d310384626444927aaca21903556039
Anshuman Kumar <mahianshuman491@gmail.com>
2023-10-18 16:54:49 +0530

    completed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <section className='CommonSection HomeSection'>
 
-      <h1 className='HomeSection-Heading'><img src={logo} alt="HomeLogo" className='HomeLogo' />React Hands-On-Four Home Page</h1>
+      <h1 className='HomeSection-Heading'><img src={logo} alt="HomeLogo" className='HomeLogo' />React Hands-On-SIX Home Page</h1>
     </section>
   )
 }
